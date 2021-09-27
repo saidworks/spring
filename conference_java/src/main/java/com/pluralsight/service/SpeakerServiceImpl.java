ec7744a8124ecdf75ea88e66fbdcdd557b1a8696
Said ZITOUNI <zitouni.sd@gmail.com>
2021-09-27 15:20:08 +0100

    conference demos java config added, need to revisit the code

---
+++ conference_java/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,25 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
+public class SpeakerServiceImpl implements SpeakerService {
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository){
+        repository = speakerRepository;
+    }
+
+    @Override
+    public List<Speaker> findAll(){
+        return repository.findAll();
+
+    }
+
+    public void setRepository(SpeakerRepository repository) {
+        this.repository = repository;
+    }
+}
